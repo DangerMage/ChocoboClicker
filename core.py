767a337765a89b8cbb2fbc3371dbd8c1bd617ab7
benjamin <benjamin+github@grusendorf.ca>
2021-07-28 16:51:26 -0600

    Added refresh button functionality

--- core.py
+++ core.py
@@ -8,6 +8,9 @@ class WindowInputs:
         self.minColumnSize = 110
         self.minRowSize = 50
         self.mouseKey = "left"
+        self.startKey = "*"
+        self.stopKey = "-"
+        self.cps = 10
         
     def window(self):
         root = tk.Tk()
@@ -37,7 +40,7 @@ class WindowInputs:
         stopKeyEntry.grid(row=1, column=2, padx=5, pady=5)
     
         #buttons
-        refreshButton = tk.Button(text="Refresh inputs", relief = tk.RIDGE, borderwidth = 5)
+        refreshButton = tk.Button(text="Refresh inputs", relief = tk.RIDGE, borderwidth = 5, command = lambda: self.refreshInputs(startKeyEntry.get, stopKeyEntry.get, clickSpeedEntry.get))
         refreshButton.grid(row=2, column=0, padx=15)
         mouseKeyToggle = tk.Button(textvariable=mouseKey, command=lambda: self.toggleMouseKey(mouseKey), relief = tk.RIDGE, borderwidth = 5, width = 5)
         mouseKeyToggle.grid(row=2, column=2)
@@ -55,3 +58,8 @@ class WindowInputs:
         else:
             self.mouseKey = "left"
             mouseKey.set(self.mouseKey)
+
+    def refreshInputs(self, startKey, stopKey, cps):
+        self.startKey = startKey
+        self.stopKey = stopKey
+        self.cps = cps
