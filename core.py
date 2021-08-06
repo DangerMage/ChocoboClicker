bc6e19e1bfe3572ead8f68deec716ded253f2068
benjamin <benjamin+github@grusendorf.ca>
2021-08-05 20:05:01 -0600

    made keybinds work

--- core.py
+++ core.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import pyautogui as clicky
+import keyboard as kb
+import time
 
 
 class WindowInputs:
@@ -8,9 +10,10 @@ class WindowInputs:
         self.minColumnSize = 110
         self.minRowSize = 50
         self.mouseKey = "left"
-        self.startKey = "*"
-        self.stopKey = "-"
+        self.startKey = '*'
+        self.stopKey = '-'
         self.cps = 10
+        self.outputText = "Started"
         
     def window(self):
         root = tk.Tk()
@@ -18,7 +21,7 @@ class WindowInputs:
 
         mouseKey = tk.StringVar()
         mouseKey.set(self.mouseKey)
-
+        outputText = tk.StringVar()
         for i in range(3):
             root.columnconfigure(i, weight=1, minsize=self.minColumnSize)
             root.rowconfigure(i, weight=1, minsize=self.minRowSize)
@@ -30,6 +33,8 @@ class WindowInputs:
         startKeyLabel.grid(row=0, column=1, padx=5, pady=5)
         stopKeyLabel = tk.Label(text="Stop Key:")
         stopKeyLabel.grid(row=0, column=2, padx=5, pady=5)
+        outputTextLabel = tk.Label(textvariable=outputText,relief=tk.GROOVE)
+        outputTextLabel.grid(row=2,column=1,padx=5,pady=5)
     
         #text boxes
         clickSpeedEntry = tk.Entry(width=8)
@@ -40,16 +45,25 @@ class WindowInputs:
         stopKeyEntry.grid(row=1, column=2, padx=5, pady=5)
     
         #buttons
-        refreshButton = tk.Button(text="Refresh inputs", relief = tk.RIDGE, borderwidth = 5, command = lambda: self.refreshInputs(startKeyEntry.get, stopKeyEntry.get, clickSpeedEntry.get))
+        refreshButton = tk.Button(text="Refresh inputs", relief=tk.RIDGE, borderwidth=5, command=lambda: self.refreshInputs(startKeyEntry.get(), stopKeyEntry.get(), clickSpeedEntry.get()))
         refreshButton.grid(row=2, column=0, padx=15)
-        mouseKeyToggle = tk.Button(textvariable=mouseKey, command=lambda: self.toggleMouseKey(mouseKey), relief = tk.RIDGE, borderwidth = 5, width = 5)
+        mouseKeyToggle = tk.Button(textvariable=mouseKey, command=lambda: self.toggleMouseKey(mouseKey), relief=tk.RIDGE, borderwidth=5, width=5)
         mouseKeyToggle.grid(row=2, column=2)
         while True:
             root.update()
             self.autoClick()
+            startKeyEntry.delete(1, 'end')
+            stopKeyEntry.delete(1, 'end')
+            clickSpeedEntry.delete(8, 'end')
+            outputText.set(f"Output:\n {self.outputText} ")
 
     def autoClick(self):
-        temp = 0
+        if kb.is_pressed(self.startKey) == True:
+            time.sleep(0.2)
+            print("debug1")
+        elif kb.is_pressed(self.stopKey) == True:
+            time.sleep(0.2)
+            print("debug2")
 
     def toggleMouseKey(self, mouseKey):
         if self.mouseKey == "left":
@@ -63,3 +77,4 @@ class WindowInputs:
         self.startKey = startKey
         self.stopKey = stopKey
         self.cps = cps
+        print(startKey,stopKey,cps)
